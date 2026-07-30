de347138c6833f089ee5a1f9207e25cec4ad67d5
Parker <parkerking0733@gmail.com>
2026-07-29 21:30:25 -0400

    Add visible bootstrap load indicator

--- payload/addons/worldbuilt_unbound/core/bootstrap.gd
+++ payload/addons/worldbuilt_unbound/core/bootstrap.gd
@@ -5,7 +5,15 @@ const LOADER_VERSION := "0.1.0"
 
 
 func _ready() -> void:
-    print("[%s] Bootstrap loaded. Version %s" % [
-        LOADER_NAME,
-        LOADER_VERSION
-    ])
+    var game_name := str(
+        ProjectSettings.get_setting("application/config/name")
+    )
+
+    DisplayServer.window_set_title(
+        "%s [Unbound %s]" % [game_name, LOADER_VERSION]
+    )
+
+    print(
+        "[%s] Bootstrap loaded. Version %s"
+        % [LOADER_NAME, LOADER_VERSION]
+    )
