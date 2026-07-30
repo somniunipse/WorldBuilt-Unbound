c2762a7170e3c685deedefd94e1d7ec9703db580
Parker <parkerking0733@gmail.com>
2026-07-29 21:24:34 -0400

    Add initial Unbound bootstrap

---
+++ payload/addons/worldbuilt_unbound/core/bootstrap.gd
@@ -0,0 +1,11 @@
+extends Node
+
+const LOADER_NAME := "Worldbuilt: Unbound"
+const LOADER_VERSION := "0.1.0"
+
+
+func _ready() -> void:
+    print("[%s] Bootstrap loaded. Version %s" % [
+        LOADER_NAME,
+        LOADER_VERSION
+    ])
