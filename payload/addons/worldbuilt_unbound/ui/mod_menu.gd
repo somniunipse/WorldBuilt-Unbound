b53223581ced632925226672a476a31ed9f09658
Parker <parkerking0733@gmail.com>
2026-07-29 22:07:34 -0400

    Scan and display installed mod archives

--- payload/addons/worldbuilt_unbound/ui/mod_menu.gd
+++ payload/addons/worldbuilt_unbound/ui/mod_menu.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 const MODS_FOLDER_NAME := "mods"
+const MOD_ARCHIVE_EXTENSION := "zip"
+const MAX_VISIBLE_MODS := 3
 
 const TITLE_POSITION := Vector2(35.0, 92.0)
 const MESSAGE_POSITION := Vector2(35.0, 118.0)
@@ -40,6 +42,9 @@ func setup(
 		5
 	)
 
+	var mod_list_label := get_node("EmptyMessage") as Label
+	mod_list_label.size = Vector2(220.0, 28.0)
+
 	_add_button(
 		"OpenFolderButton",
 		"OPEN FOLDER",
@@ -58,6 +63,8 @@ func setup(
 
 
 func open() -> void:
+	_refresh_mod_list()
+
 	for button in _main_menu_buttons:
 		button.hide()
 
@@ -143,3 +150,95 @@ func _open_mods_folder() -> void:
 		push_error(
 			"[Unbound] Could not open the mods folder."
 		)
+func _refresh_mod_list() -> void:
+	var mod_list_label := get_node_or_null(
+		"EmptyMessage"
+	) as Label
+
+	if mod_list_label == null:
+		push_warning(
+			"[Unbound] Could not find the mod list label."
+		)
+		return
+
+	var mod_files := _find_mod_archives()
+
+	if mod_files.is_empty():
+		mod_list_label.text = "NO MODS INSTALLED"
+		return
+
+	mod_list_label.text = _format_mod_list(mod_files)
+
+
+func _find_mod_archives() -> Array[String]:
+	var mod_files: Array[String] = []
+
+	var mods_directory := _get_mods_directory()
+
+	var create_error := DirAccess.make_dir_recursive_absolute(
+		mods_directory
+	)
+
+	if create_error != OK:
+		push_error(
+			"[Unbound] Could not create the mods folder."
+		)
+		return mod_files
+
+	var directory := DirAccess.open(mods_directory)
+
+	if directory == null:
+		push_error(
+			"[Unbound] Could not read the mods folder."
+		)
+		return mod_files
+
+	directory.list_dir_begin()
+
+	var file_name := directory.get_next()
+
+	while not file_name.is_empty():
+		if not directory.current_is_dir():
+			var extension := file_name.get_extension().to_lower()
+
+			if extension == MOD_ARCHIVE_EXTENSION:
+				mod_files.append(file_name)
+
+		file_name = directory.get_next()
+
+	directory.list_dir_end()
+
+	mod_files.sort()
+
+	print("[Unbound] Found ZIP files: ", mod_files)
+
+	return mod_files
+
+
+func _format_mod_list(mod_files: Array[String]) -> String:
+	var output := ""
+	var visible_count := mini(
+		mod_files.size(),
+		MAX_VISIBLE_MODS
+	)
+
+	for index in range(visible_count):
+		if not output.is_empty():
+			output += "\n"
+
+		output += mod_files[index].get_basename().to_upper()
+
+	var hidden_count := mod_files.size() - visible_count
+
+	if hidden_count > 0:
+		output += "\n+%d MORE" % hidden_count
+
+	return output
+
+
+func _get_mods_directory() -> String:
+	var game_directory := OS.get_executable_path().get_base_dir()
+
+	return game_directory.path_join(
+		MODS_FOLDER_NAME
+	)
